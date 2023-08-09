3231c2c87bf7e98a32d3f73dbe4fb63fd6d3d7e6
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-08-09 19:45:29 +0500

    Created class for add author and genre

--- book/views.py
+++ book/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from .serializers.author_serializers import AuthorSerializer
+from .serializers.genre_serializers import GenreSerializer
+
+class AddGenre(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = GenreSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Status': 'Created'}, status=status.HTTP_201_CREATED)
+        return Response({'Status': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+
+class AddAuthor(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = AuthorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Status': 'Created'}, status=status.HTTP_201_CREATED)
+        return Response({'Status': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
