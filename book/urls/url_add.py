dbdec7c627b5deb82fa1477c4c6b5771d8e33cd1
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-08-10 19:03:47 +0500

    urls folder

---
+++ book/urls/url_add.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from ..views import (
+    GenreView,
+    AuthorView,
+    PublisherView,
+    LanguageView,
+    BookView,
+    BookImageView
+)
+
+urlpatterns = [
+    path('genre/', GenreView.as_view()),
+    path('author/', AuthorView.as_view()),
+    path('publisher/', PublisherView.as_view()),
+    path('language/', LanguageView.as_view()),
+    path('book/', BookView.as_view()),
+    path('bookimage/', BookImageView.as_view()),
+ 
+]
