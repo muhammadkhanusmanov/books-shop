df0012c8ee74655d12e0b6d18b4fe52f6cf8ff27
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-09-10 11:59:00 +0500

    updated

--- book/serializers/book_serializers.py
+++ book/serializers/book_serializers.py
@@ -1,7 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from book.models import Book
+from book.serializers.genre_serializers import GenreSerializer
+from book.serializers.author_serializers import AuthorSerializer
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
+    
+    publisher = serializers.StringRelatedField()
+    language = serializers.StringRelatedField()
+    genres = GenreSerializer(many=True)
+    authors = AuthorSerializer(many=True)
+
     class Meta:
         model = Book
         fields = '__all__'
